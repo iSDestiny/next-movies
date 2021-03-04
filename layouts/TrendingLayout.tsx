bb4c164978613f96826edb16780069b71578c09b
Jason <jbugallo@uci.edu>
2021-03-03 21:02:22 -0800

    improved design of trending layout page

--- layouts/TrendingLayout.tsx
+++ layouts/TrendingLayout.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, HStack, Skeleton } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    Heading,
+    HStack,
+    Skeleton,
+    useBreakpointValue,
+    useColorMode,
+    useToken,
+    VStack
+} from '@chakra-ui/react';
 import CardGrid from 'components/CardGrid';
 import Pagination from 'components/Pagination';
 import { useEffect, useState } from 'react';
@@ -20,69 +30,132 @@ const TrendingLayout = ({
     mediaType,
     config
 }: TrendingLayoutProps) => {
+    const { colorMode } = useColorMode();
     const [selected, setSelected] = useState(0);
     const isLoading = categories[selected]?.isLoading;
     const page = categories[selected]?.page;
+    const [gray300, gray700] = useToken('colors', ['gray.300', 'gray.700']);
+    const borderColor = colorMode === 'light' ? gray300 : gray700;
+    const amountSize = useBreakpointValue({ base: 'sm', md: 'md' });
+    const headingSize = useBreakpointValue({ base: 'md', md: 'lg' });
+    const amounts = [
+        categories[0]?.trending?.total_results,
+        categories[1]?.trending?.total_results
+    ];
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     }, [page]);
 
     return (
-        <Box width="100%" maxWidth="1400px" m="auto" p="1rem">
-            <HStack spacing="1rem" justify="center" mb="1.5rem">
-                <Button colorScheme="teal" onClick={() => setSelected(0)}>
-                    Daily
-                </Button>
-                <Button colorScheme="teal" onClick={() => setSelected(1)}>
-                    Weekly
-                </Button>
-            </HStack>
-            <CardGrid
-                config={config}
-                items={categories[selected]?.trending?.results}
-                isLoading={isLoading}
-            />
-            <Skeleton
-                isLoaded={!isLoading}
-                startColor="white"
-                endColor="white"
-                minHeight="50px"
-                minWidth="100%"
+        <Box width="100%" as="main">
+            <VStack
+                width="100%"
+                as="header"
+                borderBottom={`1px solid ${borderColor}`}
+                spacing="0px"
             >
-                {!isLoading && (
-                    <>
-                        <Box
-                            display={
-                                categories[0]?.trending?.results?.length > 0 &&
-                                selected === 0
-                                    ? 'block'
-                                    : 'none'
-                            }
+                <Box width="100%" color="white" bgColor="teal.500">
+                    <HStack
+                        width="100%"
+                        justify="space-between"
+                        p="1rem"
+                        maxWidth="1400px"
+                        m="auto"
+                    >
+                        <Heading as="h1" size={headingSize}>
+                            {`Trending ${
+                                mediaType === 'movie' ? 'Movies' : 'TV Shows'
+                            }`}
+                        </Heading>
+                        <Skeleton
+                            isLoaded={!categories[selected].isLoading}
+                            startColor="white"
+                            endColor="white"
                         >
-                            <Pagination
-                                quantity={categories[0]?.trending?.total_pages}
-                                pageChangeHandler={categories[0]?.setPage}
-                                page={categories[0]?.trending?.page}
-                            />
-                        </Box>
-                        <Box
-                            display={
-                                categories[1]?.trending?.results?.length > 0 &&
-                                selected === 1
-                                    ? 'block'
-                                    : 'none'
-                            }
-                        >
-                            <Pagination
-                                quantity={categories[1]?.trending?.total_pages}
-                                pageChangeHandler={categories[1]?.setPage}
-                                page={categories[1]?.trending?.page}
-                            />
-                        </Box>
-                    </>
-                )}
-            </Skeleton>
+                            <Heading size={amountSize}>
+                                {`${amounts[selected]?.toLocaleString()} ${
+                                    mediaType === 'movie'
+                                        ? 'Movies'
+                                        : 'TV Shows'
+                                }`}
+                            </Heading>
+                        </Skeleton>
+                    </HStack>
+                </Box>
+                <HStack
+                    spacing="1rem"
+                    justify="center"
+                    p="0.5rem"
+                    align="center"
+                >
+                    <Button
+                        colorScheme="teal"
+                        onClick={() => setSelected(0)}
+                        variant={selected === 0 ? 'solid' : 'outline'}
+                    >
+                        Daily
+                    </Button>
+                    <Button
+                        colorScheme="teal"
+                        onClick={() => setSelected(1)}
+                        variant={selected === 1 ? 'solid' : 'outline'}
+                    >
+                        Weekly
+                    </Button>
+                </HStack>
+            </VStack>
+            <Box width="100%" maxWidth="1400px" m="auto" p="1rem" as="main">
+                <CardGrid
+                    config={config}
+                    items={categories[selected]?.trending?.results}
+                    isLoading={isLoading}
+                />
+                <Skeleton
+                    isLoaded={!isLoading}
+                    startColor="white"
+                    endColor="white"
+                    minHeight="50px"
+                    minWidth="100%"
+                >
+                    {!isLoading && (
+                        <>
+                            <Box
+                                display={
+                                    categories[0]?.trending?.results?.length >
+                                        0 && selected === 0
+                                        ? 'block'
+                                        : 'none'
+                                }
+                            >
+                                <Pagination
+                                    quantity={
+                                        categories[0]?.trending?.total_pages
+                                    }
+                                    pageChangeHandler={categories[0]?.setPage}
+                                    page={categories[0]?.trending?.page}
+                                />
+                            </Box>
+                            <Box
+                                display={
+                                    categories[1]?.trending?.results?.length >
+                                        0 && selected === 1
+                                        ? 'block'
+                                        : 'none'
+                                }
+                            >
+                                <Pagination
+                                    quantity={
+                                        categories[1]?.trending?.total_pages
+                                    }
+                                    pageChangeHandler={categories[1]?.setPage}
+                                    page={categories[1]?.trending?.page}
+                                />
+                            </Box>
+                        </>
+                    )}
+                </Skeleton>
+            </Box>
         </Box>
     );
 };
